725f30c41bb06481f518f9e9e212ae3ddb501171
Gaurvi Garg <garggaurvi0@gmail.com>
2026-03-26 14:17:07 +0530

    UC5: Use enhanced for-loop for multiple names

--- App/src/main/java/org/example/HelloApp.java
+++ App/src/main/java/org/example/HelloApp.java
@@ -1,22 +1,18 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
             StringBuilder names = new StringBuilder();
 
-            for (int i = 0; i < args.length; i++) {
-                names.append(args[i]);
-
-                if (i < args.length - 1) {
+            for (String name : args) {
+                if (names.length() > 0) {
                     names.append(", ");
                 }
+                names.append(name);
             }
 
             System.out.println("Hello " + names);
