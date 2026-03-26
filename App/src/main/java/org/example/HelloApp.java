8c073b07a080f1a817ac4f73c5949f9cd4ba634b
Gaurvi Garg <garggaurvi0@gmail.com>
2026-03-26 14:05:46 +0530

    UC3: Display name with default value

--- App/src/main/java/org/example/HelloApp.java
+++ App/src/main/java/org/example/HelloApp.java
@@ -3,13 +3,9 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello User");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello " + name);
     }
 }
