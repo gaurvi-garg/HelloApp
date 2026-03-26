d4b757ac54e0beff8f5b99aad993ff839ce9a260
Gaurvi Garg <garggaurvi0@gmail.com>
2026-03-26 13:58:00 +0530

    UC2: Display name using command-line argument

--- App/src/main/java/org/example/HelloApp.java
+++ App/src/main/java/org/example/HelloApp.java
@@ -2,6 +2,8 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
