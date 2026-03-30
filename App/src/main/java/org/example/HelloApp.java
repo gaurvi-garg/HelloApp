adcecb6733ac088b8cc7daff0e0d88fcde8505b5
Gaurvi Garg <garggaurvi0@gmail.com>
2026-03-30 14:36:02 +0530

    UC7: Implement greeting using String.join()

--- App/src/main/java/org/example/HelloApp.java
+++ App/src/main/java/org/example/HelloApp.java
@@ -6,15 +6,7 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String names = "";
-
-            for (String name : args) {
-                names += name + ", ";
-            }
-
-            // Remove trailing ", "
-            names = names.substring(0, names.length() - 2);
-
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
